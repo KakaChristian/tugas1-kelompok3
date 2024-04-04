8aa16a3d600cdbcbed42697985fdbf7d3c1c0a8e
AryaDwiN <nitinegaraarya@gmail.com>
2024-04-04 10:56:31 +0800

    Commit Arya Dwi Nitinegara

--- tugas1.py
+++ tugas1.py
@@ -19,3 +19,21 @@ print('Agama \t\t: ', Agama)
 print('Umur \t\t: ', Umur)
 
 print('=====================================================')
+
+nama = 'Arya Dwi Nitinegara'
+kelas = '10 RPL 2'
+absen = 6
+tgLahir = '06-02-2008'
+alamat = 'Suradadi'
+jenisKelamin = 'Laki-Laki'
+Agama = 'Islam'
+Umur = 16
+
+print('Nama \t\t: ', nama)
+print('Kelas \t\t: ', kelas)
+print('Absen \t\t: ', absen)
+print('Tanggal Lahir \t: ', tgLahir)
+print('Alamat \t\t:', alamat)
+print('Jenis Kelamin \t: ', jenisKelamin)
+print('Agama \t\t: ', Agama)
+print('Umur \t\t: ', Umur)
