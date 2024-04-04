9cd97f5a61bce7a39e85a424c640b72a97fdde9f
KakaChristian <christ123461@gmail.com>
2024-04-04 10:42:23 +0800

    Commit Christian Antony Kaka

--- tugas1.py
+++ tugas1.py
@@ -1,3 +1,5 @@
+print('================== Input Data Diri ==================')
+
 nama = 'Christian Antony Kaka'
 kelas = '10 RPL 2'
 absen = 9
@@ -5,9 +7,15 @@ tgLahir = '01-16-2008'
 alamat = 'Monjok'
 jenisKelamin = 'Laki-Laki'
 Agama = 'Kristen'
+Umur = 16
 
 print('Nama \t\t: ', nama)
 print('Kelas \t\t: ', kelas)
 print('Absen \t\t: ', absen)
 print('Tanggal Lahir \t: ', tgLahir)
 print('Alamat \t\t:', alamat)
+print('Jenis Kelamin \t: ', jenisKelamin)
+print('Agama \t\t: ', Agama)
+print('Umur \t\t: ', Umur)
+
+print('=====================================================')
