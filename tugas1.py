7b8e8a94947d965c4af2dd2ff09006e496f77d4b
KakaChristian <christ123461@gmail.com>
2024-04-04 08:38:42 +0800

    Christian Antony Kaka

---
+++ tugas1.py
@@ -0,0 +1,11 @@
+nama = 'Christian Antony Kaka'
+kelas = '10 RPL 2'
+absen = 9
+tgLahir = '01-16-2008'
+alamat = 'Monjok'
+
+print('Nama \t\t: ', nama)
+print('Kelas \t\t: ', kelas)
+print('Absen \t\t: ', absen)
+print('Tanggal Lahir \t: ', tgLahir)
+print('Alamat \t\t:', alamat)
